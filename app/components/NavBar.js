b5ff6d714115eb990db656815f0746babf66fa4f
Tamjeed Ali <tamjeedali@outlook.com>
2026-03-03 18:00:22 -0800

    improved navbar, some homepage beginning

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -2,45 +2,36 @@ import Link from 'next/link'
 
 export default function NavBar() {
     return (
-        <nav className='shadow-lg flex w-full sticky top-0 bg-white z-50 p-r pl-6'>
-
-            <ul className = "flex items-center mt-4">
-                <li className = 'h-full w-60'>
-                    <Link href="/" className="text-black text-xl hover:cursor-pointer">
-                        Sangeeta's Boutique
-                    </Link>
-                </li>
-
-                <li className='h-full w-32'>
-                    <Link href="/apparel" className="text-black text-xl hover:drop-shadow-sm hover:text-2xl transition-all duration-200 ease-in-out">
+        <nav className='shadow-lg flex w-full sticky top-0 bg-white z-50'>
+            <div className="bg-white text-black px-6 py-4 flex items-center">
+                <Link href="/">
+                    <img src="/whitelogo.png" alt="Sangeeta's Boutique logo" className="h-10" />
+                </Link>
+            </div>
+            <ul className = "flex items-center justify-end flex-1 mt-4">
+                <li className='h-full w-32 text-center'>
+                    <Link href="#apparel" className="text-black text-xl hover:drop-shadow-sm hover:text-2xl transition-all duration-200 ease-in-out">
                         Apparel
                     </Link>
                 </li>
 
-                <li className='h-full w-32'>
-                    <Link href="/accessories" className="text-black text-xl hover:drop-shadow-sm hover:text-2xl transition-all duration-200 ease-in-out">
+                <li className='h-full w-32 text-center'>
+                    <Link href="#accessories" className="text-black text-xl hover:drop-shadow-sm hover:text-2xl transition-all duration-200 ease-in-out">
                         Accessories
                     </Link>
                 </li>
-
-                <li className='h-full w-32'>
-                    <Link href="/contacts" className="text-black text-xl hover:drop-shadow-sm hover:text-2xl transition-all duration-200 ease-in-out">
-                        Contact
-                    </Link>
-                </li>
-
-                <li className='h-full w-32'>
-                    <Link href="/about" className="text-black text-xl hover:drop-shadow-sm hover:text-2xl transition-all duration-200 ease-in-out">
+                
+                <li className='h-full w-32 text-center'>
+                    <Link href="#about" className="text-black text-xl hover:drop-shadow-sm hover:text-2xl transition-all duration-200 ease-in-out">
                         About
                     </Link>
                 </li>
 
-                
-                <li className='h-full w-32'>
-                    <Link href="/cart" className="text-black text-xl hover:drop-shadow-sm hover:text-2xl transition-all duration-200 ease-in-out">
-                        Cart 
+                <li className='h-full w-32 text-center mr-4'>
+                    <Link href="#cart" className="text-black text-xl hover:drop-shadow-sm hover:text-2xl transition-all duration-200 ease-in-out">
+                        Cart
                     </Link>
-                </li>               
+                </li>
             </ul>
         </nav>
     )
--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import Fragment from "react";
 import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-   <main><NavBar></NavBar></main>
+   <main><NavBar></NavBar>
+   <h1 className="text-center text-2xl mt-4">SANJI'S BOUTIQUE</h1>
+   </main>
   );
 }
